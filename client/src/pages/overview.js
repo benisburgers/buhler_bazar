2deed93b10bdca0c49bb103887b7271d87f722c3
Beni Bar-Gera <beni@Benis-iMac.local>
2019-06-14 16:15:43 +0200

    Create html markup for profile component

--- client/src/pages/overview.js
+++ client/src/pages/overview.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 
 class Overview extends Component {
-
-
   render() {
     return (
       <h2>Overview</h2>
+
     );
   };
 }
--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -4,10 +4,33 @@ class Profile extends Component {
 
   render() {
     return (
+      <section class="profile">
+        <h2>Profile</h2>
+        <ProfileForm />
+      </section>
+    )
+  }
+}
 
-      <h2>Profile</h2>
-
-  )
+class ProfileForm extends Component {
+  render() {
+    return (
+      <form>
+        <label>
+          Profilbild
+          <input type="file"></input>
+        </label>
+        <label>
+          <input type="text" name="fullName" placeholder="Marko Lukac"></input>
+          <span style={{ whiteSpace: 'nowrap' }}>Name ändere</span>
+        </label>
+        <label>
+          <input type="text" name="email" placeholder="marko@buehler-buehler.ch"></input>
+          <span style={{ whiteSpace: 'nowrap' }}>E-Mail apasse</span>
+        </label>
+        <input type="submit" value="OKE" />
+      </form>
+    )
   }
 }
 
