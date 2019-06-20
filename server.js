538df1846ea599b5395c0466b12b3a5a8b704693
Beni Bar-Gera <beni@Benis-iMac.local>
2019-06-20 10:35:08 +0200

    Enable registration process -- check for existing user first

--- server.js
+++ server.js
@@ -12,29 +12,93 @@ const connection = mysql.createConnection({
   user: 'root',
   password: 'root',
   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-  database: 'expressreact'
+  database: 'bazar'
 })
 
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected');
-  let sql = "SELECT * FROM customers";
-  connection.query(sql, (err, result) => {
-    if (err) throw err;
-    customerData = result;
-    console.log(result);
-  })
+  // let sql = "SELECT * FROM customers";
+  // connection.query(sql, (err, result) => {
+  //   if (err) throw err;
+  //   customerData = result;
+  //   console.log(result);
+  // })
 })
 
-app.get('/api/customers', (req, res) => {
-  res.json(customerData);
-})
+// app.get('/api/customers', (req, res) => {
+//   res.json(customerData);
+// })
+
+var userExists = "lol";
 
 app.post('/api/addUser', function(request, response){
   var userInput = request.body;
-  console.log(userInput);
+  processData(userInput)
+    .then(() => {
+      response.send({ 'userExists': userExists})
+    })
 });
 
+// var processData = async (userInput) => {
+//   console.log('processData');
+//   await checkUser(userInput);
+//   if (!userExists) {
+//     createUser(userInput);
+//   }
+// }
+
+sendResponse = () => {
+  console.log('userExists: ' + userExists);
+}
+
+var processData = (userInput) => {
+  return new Promise((resolve, reject) => {
+    console.log('processData');
+    (async () => {
+      await checkUser(userInput);
+      if (!userExists) {
+        createUser(userInput);
+      }
+      resolve();
+    })();
+  })
+}
+
+checkUser = (userInput) => {
+  return new Promise((resolve, reject) => {
+    console.log('checkUser');
+    var userEmail = userInput.email;
+    console.log(userEmail);
+    var sql = "SELECT 1 FROM users WHERE email = ?"
+    connection.query(sql, [userEmail], (err, result) => {
+      if (err) throw err;
+      //if user does NOT exist
+      if (result.length == 0) {
+        console.log('USER DOES NOT EXIST');
+        userExists = false;
+        resolve();
+      }
+      //if user DOES exist
+      else {
+        console.log('USER DOES EXIST');
+        userExists = true;
+        resolve();
+      }
+    })
+  })
+}
+
+createUser = (userInput) => {
+  return new Promise((resolve, reject) => {
+    console.log('createUser');
+    var sql = "INSERT INTO users SET ?";
+    connection.query(sql, [userInput], (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    })
+  })
+}
 
 const port = 5000;
 
