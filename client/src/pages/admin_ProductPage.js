60a97802122ecafcac795186798bb89da0bd2c71
Beni Bar-Gera <beni@Benis-iMac.local>
2019-06-27 17:14:40 +0200

    Create adminProduct Page

---
+++ client/src/pages/admin_ProductPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AdminProductPage extends Component {
+  render() {
+    const { food } = this.props;
+    return (
+      <section className="admin_productPage">
+        <h2>AdminProductPage</h2>
+      </section>
+    )
+  }
+}
+
+export default AdminProductPage;
