0296cd70d32722c659ef289501874ddf929dc964
Benjamim Bar-Gera <beni@benjamims-mbp.home>
2019-08-30 10:02:13 +0200

    Add fallback image to thumbnail.js

--- client/src/components/thumbnail.js
+++ client/src/components/thumbnail.js
@@ -2,12 +2,14 @@ import React from 'react'
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 import thumbnail_icon from '../components/images/thumbnail-icon.svg';
+import alert_triangle from '../components/images/alert-triangle.svg'
 
 function Thumbnail(props) {
   const { values } = props;
+
   if (values.file) {
     return (
-      <img src={values.file} alt={values.productName}
+      <img src={values.file} alt={values.productName} onError={(e)=>{e.target.onerror = null; e.target.src=alert_triangle}}
         css={css`
           height: 100px;
           width: 100px;
