10bc57dc60cd0ccdea78f152f561c23b21b66588
Benjamim Bar-Gera <beni@Benjamims-MacBook-Pro.local>
2019-09-18 14:48:55 +0200

    create a dynamic port number in server.js to accomodate heroku

--- server.js
+++ server.js
@@ -718,6 +718,6 @@ app.get('/*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
